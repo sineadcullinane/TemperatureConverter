45a0083280d4f392919c271398dfd3850f894f2f
Sinead Cullinane <sineadcullinane@users.noreply.github.com>
2017-07-04 21:03:26 +0100

    Initial commit

---
+++ app/src/main/java/com/vogella/android/temperatureconverter/ConverterUtil.java
@@ -0,0 +1,17 @@
+package com.vogella.android.temperatureconverter;
+
+/**
+ * Created by Sinead on 04/07/2017.
+ */
+
+public class ConverterUtil {
+    // converts to celsius
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+
+    // converts to fahrenheit
+    public static float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+}
